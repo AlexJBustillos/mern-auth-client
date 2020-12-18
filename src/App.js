a2e1bdf2cfd6d2b0a53cf417ff85250f6e802569
alexjbustillos <alexjbustillos@gmail.com>
2020-12-18 12:51:03 -0500

    [navbar]updated with navLinks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome'
-
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Welcome />
     </div>
   );
