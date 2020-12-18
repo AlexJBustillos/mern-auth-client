284d33f51101a41edc06053ae5252e7be0f2432b
alexjbustillos <alexjbustillos@gmail.com>
2020-12-18 18:18:06 -0500

    Able to sign up and connect withback end

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
-const REACT_APP_SERVER_URL = process.env.REACT_APPP_SERVER_URL;
+// const REACT_APP_SERVER_URL = process.env.REACT_APPP_SERVER_URL;
+import keys from '../utils/credentials';
+const { REACT_APP_SERVER_URL } = keys
 
 const Signup = () => {
     const [name, setName] = useState('');
@@ -45,7 +47,7 @@ const Signup = () => {
     }
 
     if (redirect) return <Redirect to='/login' />
-
+    console.log(REACT_APP_SERVER_URL);
     return (
         <div className="row mt-4">
             <div className="col-md-7 offset-md-3">
